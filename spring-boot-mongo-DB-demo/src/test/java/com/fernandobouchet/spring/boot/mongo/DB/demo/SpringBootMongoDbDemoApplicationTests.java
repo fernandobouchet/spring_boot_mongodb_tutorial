c6234dbf936257f93a0ec95a57f6ffdbf4c49850
fernandobouchet <fernandobouchet@gmail.com>
2024-01-23 22:11:37 -0300

    chore: initialize project with 'spring initializr' and 'Lombok', 'Spring Web' and 'Spring Data MongoDB' dependencies

---
+++ spring-boot-mongo-DB-demo/src/test/java/com/fernandobouchet/spring/boot/mongo/DB/demo/SpringBootMongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandobouchet.spring.boot.mongo.DB.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
