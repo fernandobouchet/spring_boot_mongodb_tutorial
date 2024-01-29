9e19d0fff42b7a1bd0ae61a363cde47749388507
fernandobouchet <fernandobouchet@gmail.com>
2024-01-28 23:28:20 -0300

    feat: add a 'query' to detect when a new 'Student' is created with an already existing email.

--- spring-boot-mongo-DB-demo/src/main/java/com/fernandobouchet/spring/boot/mongo/DB/demo/SpringBootMongoDbDemoApplication.java
+++ spring-boot-mongo-DB-demo/src/main/java/com/fernandobouchet/spring/boot/mongo/DB/demo/SpringBootMongoDbDemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,12 +20,28 @@ public class SpringBootMongoDbDemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository) {
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
 		return args -> {
 			Address address = new Address("Argentina","Buenos Aires", "1710");
 			Student student = new Student("Fernando", "Bouchet","fernandobouchet@gmail.com", Gender.MALE, address, List.of("Objetos 2", "Programación funcional"), BigDecimal.TEN, LocalDateTime.now());
 
-			repository.insert(student);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(student.getEmail()));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size() > 1) {
+				throw new IllegalStateException("found a student with same email " + student.getEmail());
+			}
+
+			if(students.isEmpty()) {
+				System.out.println("Inserting student " + student);
+				repository.insert(student);
+			} else {
+				System.out.println(student + " already exists");
+			}
+
 		};
 	}
 }
+
